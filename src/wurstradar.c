02496dc8a4895c3a5df8b14e203d23c6163b6e89
David R. Piegdon <dgit@piegdon.de>
2018-02-10 23:10:43 +0000

    hw setup progress

--- src/wurstradar.c
+++ src/wurstradar.c
@@ -1,21 +1,3 @@
-/*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2014 Karl Palsson <karlp@tweak.net.au>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include <errno.h>
 #include <stdio.h>
@@ -26,6 +8,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/iwdg.h>
 
 int _write(int file, char *ptr, int len);
 
@@ -38,15 +21,31 @@ static void clock_setup(void)
 	rcc_periph_clock_enable(RCC_USART2);
 
 	rcc_periph_clock_enable(RCC_ADC1);
-	rcc_periph_clock_enable(RCC_ADC2);
 
 	rcc_periph_clock_enable(RCC_DAC);
+}
+
+static void wdt_setup(void)
+{
+	iwdg_set_period_ms(500);
+	iwdg_start();
+}
+
+static void wdt_trigger(void)
+{
+	iwdg_reset();
 }
 
 static void led_setup(void)
 {
 	// Setup LED1 on PA8.
 	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+}
+
+static void led_toggle(void)
+{
+	/* LED on/off */
+	gpio_toggle(GPIOA, GPIO8);
 }
 
 static void usart_setup(void)
@@ -90,18 +89,18 @@ int _write(int file, char *ptr, int len)
 
 static void adc_setup(void)
 {
-	// Setup ADC1_IN0 on PA0 and ADC2_IN8 on PB0.
+	// Setup ADC1_IN0 on PA0 and ADC1_IN1 on PA1.
 	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
 	gpio_mode_setup(GPIOB, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
 
 	adc_power_off(ADC1);
-	adc_power_off(ADC2);
 	adc_disable_scan_mode(ADC1);
-	adc_disable_scan_mode(ADC2);
 	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_3CYC);
-	adc_set_sample_time_on_all_channels(ADC2, ADC_SMPR_SMP_3CYC);
 	adc_power_on(ADC1);
-	adc_power_on(ADC2);
+
+	// FIXME:
+	// Setup ADC DMA transfers:
+	// ADC1 via DMA2 Stream 0 Channel 0
 }
 
 static void dac_setup(void)
@@ -117,56 +116,25 @@ static void dac_setup(void)
 static void platform_init(void)
 {
 	clock_setup();
+	wdt_setup();
 	led_setup();
 	usart_setup();
 	adc_setup();
 	dac_setup();
 }
 
-static void led_toggle(void)
-{
-	/* LED on/off */
-	gpio_toggle(GPIOA, GPIO8);
-}
-
-#if 0
-static uint16_t read_adc_naiive(uint8_t channel)
-{
-	uint8_t channel_array[16];
-	channel_array[0] = channel;
-	adc_set_regular_sequence(ADC1, 1, channel_array);
-	adc_start_conversion_regular(ADC1);
-	while (!adc_eoc(ADC1));
-	uint16_t reg16 = adc_read_regular(ADC1);
-	return reg16;
-}
-#endif
-
 int main(void)
 {
-	int i;
-#if 0
-	int j = 0;
-#endif
+	uint64_t i;
 
 	platform_init();
 
 	printf("ETAGE5 WURSTRADAR\nbasic hardware initialized.\n");
 
 	while (1) {
+		wdt_trigger();
 		led_toggle();
-#if 0
-		uint16_t input_adc0 = read_adc_naiive(0);
-		uint16_t target = input_adc0 / 2;
-		dac_load_data_buffer_single(target, RIGHT12, CHANNEL_2);
-		dac_software_trigger(CHANNEL_2);
-		uint16_t input_adc1 = read_adc_naiive(1);
-		printf("tick: %d: adc0= %u, target adc1=%d, adc1=%d\n",
-			j++, input_adc0, target, input_adc1);
-
-
-#endif
-		for (i = 0; i < 1000000; i++) { /* Wait a bit. */
+		for (i = 0; i < 2000000; i++) { /* Wait a bit. */
 			__asm__("NOP");
 		}
 	}
